d27e4cb930bf99e7e4278b8db96b5646284eca77
LeCanhGiangThi <giangthipht@gmail.com>
2024-06-13 22:02:05 +0700

    Add files via upload

---
+++ functionproduct.js
@@ -0,0 +1,23 @@
+function generateProductHTML(product) {
+  const discount = product.discount ? Math.floor(product.discount * 100) + '%';
+  const discountedPrice = product.discount ? product.price * (1 - product.discount) : product.price;
+  const imageSrc = product.image ? `data:image/${product.image.type};base64,${product.image.data}`;
+
+  return `
+    <div class="product">
+      <div class="product-image">
+        <img src="${imageSrc}" alt="${product.name}">
+        ${discount ? `<span class="discount-badge">${discount}</span>` : ''}
+      </div>
+      <div class="product-info">
+        <h3>${product.name}</h3>
+        <p class="short-desc">${product.short_desc}</p>
+        <p class="product-tag">${product.tag ? product.tag: ""}</p>
+        <div class="product-price">
+          <span class="price">${discountedPrice.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</span>
+          ${product.unit_price ? `<span class="unit-price">/${product.unit_price}</span>` : ''}
+        </div>
+      </div>
+    </div>
+  `;
+}
