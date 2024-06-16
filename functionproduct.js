4124fdc1ad9265888fc402d4526359e58b3be76a
LeCanhGiangThi <giangthipht@gmail.com>
2024-06-16 12:00:36 +0700

    Add files via upload

--- functionproduct.js
+++ functionproduct.js
@@ -1,12 +1,14 @@
 function generateProductHTML(product) {
-  const discount = product.discount ? Math.floor(product.discount * 100) + '%';
+  const discount = product.discount ? Math.floor(product.discount * 100) + '%' : '10%';
   const discountedPrice = product.discount ? product.price * (1 - product.discount) : product.price;
-  const imageSrc = product.image ? `data:image/${product.image.type};base64,${product.image.data}`;
+  const imageSrc = product.image ? `data:image/${product.image.type};base64,${product.image.data}`: 'sofa.png';
+  // const imageSrc = product.image ? `${product.image.src}`: 'sofa.png';
 
   return `
     <div class="product">
       <div class="product-image">
         <img src="${imageSrc}" alt="${product.name}">
+        <br>
         ${discount ? `<span class="discount-badge">${discount}</span>` : ''}
       </div>
       <div class="product-info">
